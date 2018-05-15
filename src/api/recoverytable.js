eedb4497fca12537b038385a690dd50208c1626a
jack jo <jackjo@jackdeMBP.lan>
2018-05-15 08:46:57 +0800

    fix

--- src/api/recoverytable.js
+++ src/api/recoverytable.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getList(params) {
   return request({
-    baseURL: 'vbs',
+    baseURL: 'http://59.110.238.148:3000',
     url: '/vbstable/query',
     method: 'get',
     params
@@ -11,7 +11,7 @@ export function getList(params) {
 
 export function getListHistory(params) {
   return request({
-    baseURL: 'vbs',
+    baseURL: 'http://59.110.238.148:3000',
     url: '/vbstable/query',
     method: 'get',
     params
@@ -20,7 +20,7 @@ export function getListHistory(params) {
 
 export function getListDetail(params) {
   return request({
-    baseURL: 'vbs',
+    baseURL: 'http://59.110.238.148:3000',
     url: '/vbstable/query',
     method: 'get',
     params
--- src/api/seetable.js
+++ src/api/seetable.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getList(params) {
   return request({
-    baseURL: 'vbs',
+    baseURL: 'http://59.110.238.148:3000',
     url: '/vbstable/query',
     method: 'get',
     params
@@ -11,7 +11,7 @@ export function getList(params) {
 
 export function getListAll(params) {
   return request({
-    baseURL: 'vbs',
+    baseURL: 'http://59.110.238.148:3000',
     url: '/vbstableall/query',
     method: 'get',
     params
@@ -20,7 +20,7 @@ export function getListAll(params) {
 
 export function getListDetail(params) {
   return request({
-    baseURL: 'vbs',
+    baseURL: 'http://59.110.238.148:3000',
     url: '/vbstableall/query',
     method: 'get',
     params
